071b86e4fdd196092bfcbd1e6d124bfa3dbeddc4
constancecchen <constance.chen.3@gmail.com>
2016-10-15 14:26:22 -0700

    Minor JS cleanup

--- js/main.js
+++ js/main.js
@@ -1,103 +1,105 @@
-/*
-	var gridBox = $("#js-foodContainer");
+$(document).ready(function() {
 
-	var estimate = $("#estimateButton");
-	estimate.addEventListener("mousedown", pourHandler, false);
-*/
+	/**
+	 * Set up
+	 */
+	var total = 0;
+	var pourPercent = 0;
+	var isPouring = false;
+	var pourTimer;
 
-var pourPercent = 0;
-var isPouring = false;
-var pourTimer;
+	/**
+	 * Increment / decrement logic
+	 */
+	$('.js-subtract').click(function(){
+		
+		var quantity = $(this).siblings(".js-quantity");
+		var num = parseInt(quantity.text());
+		if(num > 0){
+			quantity.text(--num);
+			total -= $(this).closest(".js-food-item").data("value");
+		}
+	});
 
-var selections = [];
-var total = 0;
+	$('.js-add').click(function(){
+		
+		var quantity = $(this).siblings(".js-quantity");
+		var num = parseInt(quantity.text());
+		if(num < 10){
+			quantity.text(++num);
+			total += $(this).closest(".js-food-item").data("value");
+		}
+	});
 
-$('.js-subtract').click(function(){
-	
-	var quantity = $(this).siblings(".js-quantity");
-	var num = parseInt(quantity.text());
-	if(num > 0){
-		quantity.text(--num);
-		total -= $(this).closest(".js-food-item").data("value");
-	}
+	/**
+	 * "Page" navigation
+	 */
+	$(".js-estimate").click(function(){
+		$(".js-main").hide();
+		$(".js-estimating").show();
+		pourHandler();
+	});
 
-	console.log(total);
-});
+	$(".js-estimate-stop").click(function(){
+		pourHandler();
 
-$('.js-add').click(function(){
-	
-	var quantity = $(this).siblings(".js-quantity");
-	var num = parseInt(quantity.text());
-	if(num < 10){
-		quantity.text(++num);
-		total += $(this).closest(".js-food-item").data("value");
-	}
+		setTimeout(function() {
+			$(".js-estimating").hide();
+			$(".js-sugar-reveal").show();
+	  }, 500);
+	});
 
-	console.log(total);
-});
+	/**
+	 * Pouring logic
+	 */
+	var pourHandler = function(){
+		if(isPouring){
+			isPouring = false;
+			clearInterval(pourTimer);
+		} else {
+			if(pourPercent < 100){
+				isPouring = true;
 
-/**
- * "Page" navigation
- */
-$(".js-estimate").click(function(){
-	console.log(total);
+				pourTimer = setInterval(function(){
+					$(".js-measuring-level").css({
+						"transform": "scaleY(" + pourPercent / 100 + ")"
+					});
 
-	$(".js-main").hide();
-	$(".js-estimating").show();
-	pourHandler();
-});
-
-$(".js-estimate-stop").click(function(){
-	pourHandler();
-
-	$(".js-estimating").hide();
-	$(".js-sugar-reveal").show();
-});
-
-function pourHandler(){
-	if(isPouring){
-		isPouring = false;
-		clearInterval(pourTimer);
-	} else {
-		if(pourPercent < 100){
-			isPouring = true;
-
-			pourTimer = setInterval(function(){
-				if(pourPercent < 100){
-					pourPercent++;
-				} else {
-					pourTimer = false;
-				}
-
-				$(".js-measuring-level").css({
-					"transform": "scaleY(" + pourPercent / 100 + ")"
-				});
-
-			},40);
+					if(pourPercent < 100){
+						pourPercent++;
+					} else {
+						pourTimer = false;
+					}
+				}, 40);
+			}
 		}
 	}
-}
-    
-function gradeUser(score){
-	var maxScore = 100;
-	var percent = score / maxScore * 100;
 
-	var gradeArr = [60,70,80,90,95];
-	var rankArr = ["F for FAILURE","C","B","A","A+",];
+	/**
+	 * Final grading math
+	 */
+	var gradeUser = function(score){
+		var maxScore = 100;
+		var percent = score / maxScore * 100;
 
-	var curRank;
+		var gradeArr = [60,70,80,90,95];
+		var rankArr = ["F for FAILURE","C","B","A","A+",];
 
-	if(percent <= gradeArr[0]){
-		curRank = rankArr[0];
-	} else if(percent <= gradeArr[1]){
-		curRank = rankArr[1];
-	} else if(percent <= gradeArr[2]){
-		curRank = rankArr[2];
-	} else if(percent <= gradeArr[3]){
-		curRank = rankArr[3];
-	} else {
-		curRank = rankArr[4];
+		var curRank;
+
+		if(percent <= gradeArr[0]){
+			curRank = rankArr[0];
+		} else if(percent <= gradeArr[1]){
+			curRank = rankArr[1];
+		} else if(percent <= gradeArr[2]){
+			curRank = rankArr[2];
+		} else if(percent <= gradeArr[3]){
+			curRank = rankArr[3];
+		} else {
+			curRank = rankArr[4];
+		}
+
+		return curRank;
 	}
 
-	return curRank;
-}
+});
