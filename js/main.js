fad6b12f51493a134ae6e0c93b032f18f5faba06
Daniel Pierce IV <Daniel.Pierce.IV@gmail.com>
2016-10-15 12:51:37 -0700

    Added total

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 	var gridBox = $("#js-foodContainer");
 
 	var estimate = $("#estimateButton");
+	estimate.addEventListener("mousedown", pourHandler, false);
 */
 
 var pourPercent = 0;
@@ -9,81 +10,14 @@ var isPouring = false;
 var pourTimer;
 
 var selections = [];
-var foodArr = [];
+var total = 0;
 
-foodArr.push({
-	name: "Sugary Cereal",
-	serving: "3/4 cup",
-	sugar: 9
+$('.js-subtract').click(function(){
+	total -= $(this).closest(".js-food-item").data("value");
+	console.log(total);
 });
 
-foodArr.push({
-	name: "Fruit Flavored Drink",
-	serving: "1 pouch",
-	sugar: 16
-});
-
-foodArr.push({
-	name: "Flavored Yogurt",
-	serving: "1 container",
-	sugar: 16
-});
-
-foodArr.push({
-	name: "Soda",
-	serving: "1 can",
-	sugar: 38
-});
-
-foodArr.push({
-	name: "Granola Bar",
-	serving: "1 bar",
-	sugar: 7
-});
-
-foodArr.push({
-	name: "Chocolate Milk",
-	serving: "1 cup",
-	sugar: 14
-});
-
-foodArr.push({
-	name: "Muffin",
-	serving: "1 muffin",
-	sugar: 13
-});
-
-foodArr.push({
-	name: "Pasta Sauce",
-	serving: "1/2 cup",
-	sugar: 8
-});
-
-foodArr.push({
-	name: "Graham Crackers",
-	serving: "2 crackers",
-	sugar: 7
-});
-
-foodArr.push({
-	name: "Cupcake",
-	serving: "1 piece",
-	sugar: 20
-});
-
-
-foodArr.push({
-	name: "Fruit Snacks",
-	serving: "1 pouch",
-	sugar: 18
-});
-
-foodArr.push({
-	name: "Sports Drink",
-	serving: "1 355ml bottle",
-	sugar: 21
-});
-
+function pourHandler(){
 	if(isPouring){
 		isPouring = false;
 		clearInterval(pourTimer);
